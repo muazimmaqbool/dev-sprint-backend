2bce98fd1453bffc5386ea69dfd954b0efa45ca4
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-01-19 22:04:36 +0530

    info about middlewares

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT=process.env.PORT
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+//Read about these middlewares in bottom
 
 //-> db:
 
@@ -24,3 +25,28 @@ app.get("/",(req,res)=>{
 app.listen(PORT,()=>{
     console.log("Server is running on: http://localhost:3000")
 })
+
+/*
+->About middlewares:
+
+1)app.use(cors());  used to enable CORS
+
+2)app.use(express.json());
+->used to 'Parse' incoming requests with 'JSON' payloads.
+with this we can do this: req.body.name
+Used for: POST requests, PUT/PATCH requests, APIs sending JSON data
+
+3)app.use(express.urlencoded({extended:true}))
+->Parses URL-encoded data (form submissions).
+extended: true means: Allows nested objects and complex data structures
+->Uses the qs library internally
+Example: user[name]=Muazim&user[age]=23
+        becomes:
+        {
+            user: {
+                name: "Muazim",
+                age: 23
+            }
+        }
+
+*/
