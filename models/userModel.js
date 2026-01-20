7be3d65a0e4ab760da5cbee7941976c576d3cafd
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-01-20 21:51:51 +0530

    comments added

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema=new mongoose.Schema({
     name:{
         type:String,
         required:true,
-        trim:true,
+        trim:true, //removes extra spaces from the beginning and end of the string.
     },
     email:{
         type:String,
@@ -19,6 +19,20 @@ const userSchema=new mongoose.Schema({
 },{
     timestamps:true
 })
+
+/*
+->timeStamps:true
+Mongoose automatically adds two fields to every document:
+    {
+    createdAt: Date
+    updatedAt: Date
+    }
+
+    createdAt → set once when document is created
+    updatedAt → updates every time you update the document
+*/
+
+
 const User=mongoose.model("User",userSchema)
 export default User
 //or
