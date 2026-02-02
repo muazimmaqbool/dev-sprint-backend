9098f8eeadb3bbe09bc1e4ea0dc56184e5de361b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-02 22:06:49 +0530

    userRoute added

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { login, register } from "../controllers/userController";
+
+const userRouter = express.Router();
+
+//used to add new user
+userRouter.post("/register", register);
+
+//route used for login
+userRouter.post("/login", login);
+
+export default userRouter;
